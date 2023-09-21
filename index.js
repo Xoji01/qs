875e67e6b3023762545f526dcaf70f6133fa5c4b
Xoji01 <135627056+Xoji01@users.noreply.github.com>
2023-09-21 13:52:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let name = prompt ('w')
+let arr = ['xojik', 'aziza', 'safir', 'samira'] 
+if(name>=0&& name <arr.length) {arr.splice(name-1,1)
+    alert('Осталось ' + arr);}
+    else {alert('Ошибка')}
+    
